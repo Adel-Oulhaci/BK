9e419f915fbd54d383524904f67e149d337fc85d
Adel Fakhr EL Islem OULHACI <35428516+Adel-Oulhaci@users.noreply.github.com>
2024-11-07 19:09:22 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN9Mitu2uUbYscNE0hNHXHDLODlEXdzYQ",
-  authDomain: "bkweb-630de.firebaseapp.com",
-  projectId: "bkweb-630de",
-  storageBucket: "bkweb-630de.firebasestorage.app",
-  messagingSenderId: "439322467395",
-  appId: "1:439322467395:web:c3b04eae424c525ab648f6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
